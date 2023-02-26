baca680b36083f1952a90b728949dfc2b3b9444c
Juan Francisco Aguirre <jf_aguirre@hotmail.com>
2023-02-26 06:39:23 +0100

    updated title chopping for long titles

--- src/app/products/Product.js
+++ src/app/products/Product.js
@@ -6,7 +6,7 @@ export const Product = ({ title, presentation, price, added }) => {
     <>
       <div
         className={clsx(
-          'flex flex-col items-center shadow dark:shadow-none rounded-md border dark:border-grayish-800 p-2 gap-4 h-40',
+          'flex flex-col items-center shadow dark:shadow-none rounded-md border dark:border-grayish-800 p-2 gap-4 h-48',
           added ? 'bg-primary-600' : 'bg-grayish-100 dark:bg-grayish-900',
         )}
       >
@@ -18,26 +18,29 @@ export const Product = ({ title, presentation, price, added }) => {
             {String(title.split(' ')[0][0]).toUpperCase()}
           </Text>
         </div>
-        <div className="flex flex-col gap-2 h-20">
+        <div className="flex flex-col gap-2 h-20 grow">
           <Text
             className={clsx(
               added ? 'text-black' : 'dark:font-light',
-              'text-sm basis-2/5 text-center',
+              'text-sm basis-[60%] text-center',
             )}
           >
-            {title}
+            {`${title
+              .split('')
+              .map((char, i) => (i <= 23 ? char : null))
+              .join('')}${title.length > 24 ? '...' : ''}`}
           </Text>
           <Text
             className={clsx(
               !added && 'dark:font-light',
-              'text-grayish-400 text-sm basis-2/5 text-center italic',
+              'text-grayish-400 text-sm basis-[20%] text-center italic',
             )}
           >
             {presentation}
           </Text>
           <Text
             className={clsx(
-              'basis-1/5 text-right pr-1 font-semibold',
+              'basis-[20%] text-right pr-1 font-semibold',
               added && 'text-white',
             )}
           >
